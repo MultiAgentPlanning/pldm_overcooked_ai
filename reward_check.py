01b3d1956a3cf1225d2308998c466aaff3871e55
Ayush Jhaveri <arhjhaveri@gmail.com>
2025-05-02 15:24:42 -0400

    more reward results

--- reward_check.py
+++ reward_check.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load your CSV file
-df = pd.read_csv("output_dir/simulation_log_2020_run_18_overcooked_discrete_sac_20250430193519_PH1_NS1_MS400_LT1.0.csv")
+df = pd.read_csv("output_dir/simulation_log_2020_run_18_overcooked_cql_20250501200656_PH100_NS100_MS400_LT1.0.csv")
 
 # Group by sample_idx and sum the reward
 reward_sums = df.groupby("sample_idx")["reward"].sum()
